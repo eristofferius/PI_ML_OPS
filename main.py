2c923961b1bad24ac845c5b9d299c953407f5789
eristofferius <cristopher.moralesceron@outlook.com>
2023-11-26 11:45:07 -0300

    requeriments fix 25

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+#app = FastAPI()
 
 #http://127.0.0.1:8000
 
-@app.get("/inicio")
-async def ruta_prueba():
-    return {"message" : "TEST"}
-
-
-#app = FastAPI()
-
-
 #@app.get("/inicio")
 #async def ruta_prueba():
-#    return "Hola"
+ #   return {"message" : "TEST"}
+
+
+app = FastAPI()
+
+
+@app.get("/inicio")
+async def ruta_prueba():
+    return "Hola"
 
 #def developer( desarrollador : str ): Cantidad de items y porcentaje de contenido Free por año según empresa desarrolladora. Ejemplo de retorno:
 
